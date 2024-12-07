293c196a26609329e1a1a7de49fb5ca14bb40bf7
mani <manirazeghi049@gmail.com>
2024-12-07 01:28:24 -0800

    create class and add method

---
+++ calcut.py
@@ -0,0 +1,11 @@
+
+
+class Calculator:
+
+    def __init__(self, number1: int, number2: int) -> None:
+        self.number1 = number1
+        self.number2 = number2
+
+
+    def add(self) -> int:
+        return self.number1 + self.number2
